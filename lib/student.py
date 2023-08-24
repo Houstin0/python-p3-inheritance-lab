32932ad22ae34c5ed54fa1174c990d514b59c81c
Houstin Angwenyi <houstin.angwenyi@student.moringaschool.com>
2023-08-24 19:48:04 +0300

    completed assignment

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,13 @@
 from user import User
 
 class Student(User):
+
+    def __init__(self, first_name, last_name,knowledge=[]):
+        self.knowlegde=knowledge
+        super().__init__(first_name, last_name)
     
-    def learn(self):
-        pass
+    def learn(self,new_information):
+        self.knowlegde.append(new_information)
+
+john=Student("john","doe")
+print(hasattr(john,"knowlegde"))
